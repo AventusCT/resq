2c0f62cf1fd71e58a4ccc1987b00f0cb469a5c8b
ldaventus <spijkerslaag@gmail.com>
2025-09-26 18:39:19 +0200

    style css en reser php
    doet niks met de db

---
+++ magazijn/public/reservation.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Status van bestelling veranderen</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="magazijn/public/js/script.js"></script>
+    <script src="magazijn/public/js/reservation.js"></script>
+</head>
+<body>
+    <div class="statusverandercontainer">
+        <h1>Status van bestelling veranderen</h1>
+        <form id="reservationForm">
+            <label for="reservationId">Bestelling ID:</label>
+            <input type="text" id="reservationId" name="reservationId" readonly>
+
+            <label for="status">Status wisselen:</label>
+            <select id="status" name="status">
+                <option value="reserveren">Reserveren</option>
+                <option value="ophalen">Ophalen</option>
+                <option value="retour">Terughalen</option>
+            </select>
+
+            <button class="statusproduct"> Update productstatus</button>
+        </form>
+        <div id="message"></div>
+    </div>
+</body>
+</html>

968e1d1a092b42d94b4a29eea9ea550779b3ad5b
ldaventus <spijkerslaag@gmail.com>
2025-11-21 12:32:51 +0100

    Update header.php

--- magazijn/public/Includes/header.php
+++ magazijn/public/Includes/header.php
@@ -3,7 +3,7 @@
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Webwinkel L.D.H</a>
+        <a class="navbar-brand" href="index.php">ResQ</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
